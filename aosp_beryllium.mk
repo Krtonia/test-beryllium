1ddc2a7002d2640b2da7db4409a78e037948af01
krtonia <krtonia@gmail.com>
2022-07-17 10:20:28 +0530

    beryllium: Adapt to PixelExtende

--- aosp_beryllium.mk
+++ aosp_beryllium.mk
@@ -6,19 +6,18 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Evolution-X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Evolution-X flags
-EVO_BUILD_TYPE := UNOFFICIAL
-TARGET_SUPPORTS_QUICK_TAP := true
+# PixelExtended Official Stuff
+PEX_BUILD_TYPE := UNOFFICIAL
+PEX_MAINTAINER := Excraze
 TARGET_FACE_UNLOCK_SUPPORTED := true
-
-# Bootanimation Resolution
-TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_BOOT_ANIMATION_RES=1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := evolution_beryllium
+PRODUCT_NAME := aosp_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
@@ -26,10 +25,11 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_SYSTEM_NAME := beryllium
 
-BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V12.0.3.0.QEJMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:12/SQ3A.220605.009.A1/8643238:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.3.0.QEJMIXM release-keys" \
+    PRIVATE_BUILD_DESC="redfin-user 12 SQ3A.220605.009 8643238 release-keys" \
     TARGET_PRODUCT="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
